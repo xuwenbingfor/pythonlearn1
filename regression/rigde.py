1e95a13f42239a8887326c36dc41324ec3adbf3f
xuwenbingfor <1965104943@qq.com>
2020-02-22 13:36:11 +0800

    模型评估方法

--- regression/rigde.py
+++ regression/rigde.py
@@ -1,3 +1,8 @@
+# 岭回归：一种将L2作为正则化工具的线性最小二乘回归。
+# 在岭回归中，模型会保留所有的特征变量，但是会减小特征变量的系数值，让特征变量对预测结果的
+# 影响变小， 在岭回归中是通过改变其alpha 参数来控制减小特征变量系数的程度。而这
+# 种通过保留全部特征变量，只是降低特征变量的系数值来避免过拟合的方法，我们称之为L2 正则化。
+
 from sklearn.datasets import load_boston
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
