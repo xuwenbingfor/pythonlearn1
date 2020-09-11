ec15fb036d699d25d704201d4c9fb2cd93a45590
xuwenbingfor <1965104943@qq.com>
2020-09-11 22:55:22 +0800

    list学习

---
+++ base/py_base_1.py
@@ -0,0 +1,18 @@
+# 1、列表
+# list1 = [1, 2, 3]
+# list1[0] = -1
+# print(list1)
+# list1[1:-1] = [-2, -3, -4]
+# print(list1)
+
+# list1.pop(1)
+# print(list1)
+
+# print(3 in list1)
+
+# print(list1.index(3))
+
+# print(list1.count(1))
+
+# 2、元组
+# 3、集合
