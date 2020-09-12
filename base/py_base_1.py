07db76717a43ee150e65c44c0c73210b707f4445
xuwenbingfor <1965104943@qq.com>
2020-09-12 22:02:09 +0800

    元组、集合学习

--- base/py_base_1.py
+++ base/py_base_1.py
@@ -15,4 +15,26 @@
 # print(list1.count(1))
 
 # 2、元组
+# 原始功能
+# (one, two, three, four) = (1, 2, 3, 4)
+# print(one)
+# 等号右侧的值会被打包为元组，然后拆包到左侧的变量中中
+# one, two, three, four = 1, 2, 3, 4
+# print(two)
+# 扩展拆包，允许带*的元素接收任意数量的未匹配元素
+# one, two, *three = 1, 2, 3, 4
+# print(one)
+# print(three)
+# print(type(three))
+
 # 3、集合
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# print(type(set1))
+# print(type(set2))
+# print(set1 | set2)
+# print(set1 & set2)
+
+# 4、字符串
+print(','.join(['a', 'b', 'c']))
+print('a,b,c'.split(','))
