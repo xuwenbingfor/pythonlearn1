861892d0296ecd92614862dae89591a73780bf81
xuwenbingfor <1965104943@qq.com>
2020-10-12 14:51:33 +0800

    py复习

--- pd/pd3.py
+++ pd/pd3.py
@@ -1,11 +1,13 @@
 # 数据源读写
 # DataFrame
-# 常见属性values、index、columns、dtypes
-# 查：loc[行索引名称(未指定index时是行索引位置)或条件,列索引名称]\iloc[行索引位置,列索引位置]
-# 改：原理
-# 增/删
-# 统计：https://www.jianshu.com/p/360c69f0083e
-
+## 常见属性values、index、columns、dtypes
+## 查：loc[行索引名称(未指定index时是行索引位置)或条件,列索引名称]\iloc[行索引位置,列索引位置]
+## at:https://blog.csdn.net/sinat_29675423/article/details/87975489
+## 改：原理
+## 增/删
+## 统计：https://www.jianshu.com/p/360c69f0083e
+#Series
+## get
 import pandas as pd
 import numpy as np
 
